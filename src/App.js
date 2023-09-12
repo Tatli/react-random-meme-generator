3385b0e3dc216cfb7af12613ac4415896947dad6
Osman Tatli <tatli.osman@gmx.at>
2023-09-12 18:16:24 +0200

    Move files to correct repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-import ExampleConditionalRenderingBooleanOperator from '.ExampleConditionalRenderingBooleanOperator';
-import ExampleConditionalRenderingTernaryOperator from '.ExampleConditionalRenderingTernaryOperator';
-
 export default function App() {
   return (
     <>
-      <ConditionalRenderingTernaryOperator />
-      <hr />
-      <ConditionalRenderingBooleanOperator />
+      <h2>Random Meme Generator</h2>
     </>
   );
 }
